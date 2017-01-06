de1705a1c0290f627ac895f5fb20df03f353913b
dnizetic <denis_nizetic@hotmail.com>
2017-01-06 15:54:39 +0100

    email setup

--- application/config/email.php
+++ application/config/email.php
@@ -10,10 +10,17 @@
 |	http://codeigniter.com/user_guide/libraries/email.html
 |
 */
-$config['mailtype'] = 'html';
-$config['charset'] = 'utf-8';
-$config['newline'] = "\r\n";
 
+$config['protocol']     = 'smtp';
+$config['smtp_host']    = 'ssl://smtp.googlemail.com';
+$config['smtp_port']    = 465;
+$config['smtp_timeout'] = '30';
+$config['smtp_user']    = 'dialemail12345@gmail.com';
+$config['smtp_pass']    = '123123123qwe';
+
+$config['mailtype'] = 'html';
+$config['charset'] = 'iso-8859-1';
+$config['newline'] = "\r\n";
 
 /* End of file email.php */
 /* Location: ./application/config/email.php */
