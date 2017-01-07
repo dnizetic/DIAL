ebf5a68e65a26e406fda56851f387e35ccb43708
dnizetic <denis_nizetic@hotmail.com>
2017-01-07 17:09:59 +0100

    rename home controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Welcome extends MY_Controller {
+class Home extends MY_Controller {
 
     function __construct() {
         parent::__construct();
